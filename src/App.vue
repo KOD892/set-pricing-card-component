47f2ff4ed4f0d48a058b38f36ef1710da7308e3e
KOD892 <danladi@localhost.localdomain>
2025-07-06 15:42:19 +0200

    initial comit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template lang="">
+     <div class="card flex f-column">
+        <div class="card-header grid grid-three">
+            <div class="option" id="basic">
+                   Basic 
+            </div>
+            <div  id="standard" class="option selected">
+                  Standard
+            </div>
+            <div class="option" id="premium">
+                Premium
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="price pb-1">
+                <p> STANDARD</p>
+                <h3 class="flex"><span class="">Icon Sets</span><span class="">$120</span></h3>
+            </div>
+            <div class="details">
+            <div class="pb-1" id="description">              Up to 100 creative & professional icons + two color version/themes per month
+            </div>
+                <div class="checklist">
+                    <ul>
+                        <li>Included Source files</li>
+                        <li>Converted to responsive componnet</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="footer pt-2 center">
+                <button class="pt-1 pb-1">Add to Cart</button>
+            </div>
+        </div>
+        
+    </div>
+</template>
+<script setup>
+
+
+</script>
+<style lang="">
+    
+</style>
